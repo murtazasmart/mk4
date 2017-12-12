7c620284952c7170d57b8832ac3d4fc3331d5e34
murtazasmart <murtazasmart@hotmail.com>
2017-12-12 10:46:37 +0530

    changes made for challenge 3

--- game.test.js
+++ game.test.js
@@ -16,42 +16,67 @@ describe("Test", () => {
   });
 })
 
-describe("Test Armour class", () => {
-  describe("Test armorbonus field", () => {
-    it("Bonus shouldnt be 0", () => {
-      var spineArmor = new Armor("Spine Armor", 6);
-      var spineChain = new Weapon("Spine Chain", 5);
-      Scorpion = new Warrior("Scorpion", 7, 7, [spineArmor, spineChain]);
-      // console.log(Scorpion.armorBonus);
-      expect(Scorpion.armorBonus).not.be.equal(0);
-    });
-  });
+describe("Testing isolation of Warrior Class", () => {
+  var spineArmor
+  var spineChain
+  var scorpion
+  beforeEach(() => {
+    spineArmor = new Armor("Spine Armor", 6);
+    spineChain = new Weapon("Spine Chain", 5);
+    scorpion = new Warrior("Scorpion", 7, 7, [spineArmor, spineChain]);
+  })
 
-  describe("rollDice function", () => {
-    it("should not return 0", () => {
-      for (i = 1; i <= 1000; i++) {
-        var num = Warrior.rollDice(i, 3)
-        // console.log(num)
-        expect(num).be.not.equal(0);
-      }
+  it("should initalize warrior successfully", () => {
+    expect(scorpion instanceof Object).should.be.true;
+  })
 
-    })
-
-    it("should be finite number", () => {
-      for (i = 1; i <= 1000; i++) {
-        var num = Warrior.rollDice(i, 3)
-        expect(_.isFinite(num)).should.be.true;
-        
-      }
-      
-    })
-
-    it("should be a valid number", () => {
-      for (i = 1; i <= 1000; i++) {
-        var num = Warrior.rollDice(i, 3)
-        expect(isNaN(num)).should.be.not.true;
-      }
-    })
+  it("checks if initialized armour bonus is equal to 6", () => {
+    expect(scorpion.armorBonus).to.be.equal(6);
+  })
+  afterEach(() => {
+    spineArmor = undefined;
+    spineChain = undefined;
+    scorpion = undefined;
 
   })
-});
+})
+
+// describe("Test Armour class", () => {
+//   describe("Test armorbonus field", () => {
+//     it("Bonus shouldnt be 0", () => {
+//       var spineArmor = new Armor("Spine Armor", 6);
+//       var spineChain = new Weapon("Spine Chain", 5);
+//       Scorpion = new Warrior("Scorpion", 7, 7, [spineArmor, spineChain]);
+//       // console.log(Scorpion.armorBonus);
+//       expect(Scorpion.armorBonus).not.be.equal(0);
+//     });
+//   });
+
+//   describe("rollDice function", () => {
+//     it("should not return 0", () => {
+//       for (i = 1; i <= 1000; i++) {
+//         var num = Warrior.rollDice(i, 3)
+//         // console.log(num)
+//         expect(num).be.not.equal(0);
+//       }
+
+//     })
+
+//     it("should be finite number", () => {
+//       for (i = 1; i <= 1000; i++) {
+//         var num = Warrior.rollDice(i, 3)
+//         expect(_.isFinite(num)).should.be.true;
+
+//       }
+
+//     })
+
+//     it("should be a valid number", () => {
+//       for (i = 1; i <= 1000; i++) {
+//         var num = Warrior.rollDice(i, 3)
+//         expect(isNaN(num)).should.be.not.true;
+//       }
+//     })
+
+//   })
+// });
